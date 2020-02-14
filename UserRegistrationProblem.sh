6814706cf1f0e07e8735ab9fe8b348d334687168
kalyaniRane <kalyanirane19@gmail.com>
2020-02-14 17:05:44 +0530

    ADDED : Functionality To Check User's Last Name

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,21 +1,25 @@
-#!/bin/bash -x
+#!/bin/bash
 shopt -s extglob
 
 echo "Welcome To RegEx Problems"
 
-echo "Enter First Name"
-read firstName
-
-validFirstName="^[A-Z][a-z]{3,}$"
+validName="^[A-Z][a-z]{3,}$"
 
 function checkValid()
 {
 	local name=$1
-	if [[ $name =~ $validFirstName ]]
+	if [[ $name =~ $validName ]]
 	then
 			echo "Valid Name"
 	else
 			echo "Invalid Name"
+			exit
 	fi
 }
+
+echo "Enter First Name: "
+read firstName
 checkValid $firstName
+echo "Enter Last Name: "
+read lastName
+checkValid $lastName
