e613d1e529dff855acfd94b40bf5c2740911534e
kalyaniRane <kalyanirane19@gmail.com>
2020-02-14 16:25:28 +0530

    ADDED : Function To Validate A User's First Name

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -3,3 +3,19 @@ shopt -s extglob
 
 echo "Welcome To RegEx Problems"
 
+echo "Enter First Name"
+read firstName
+
+validFirstName="^[A-Z][a-z]{3,}$"
+
+function checkValid()
+{
+	local name=$1
+	if [[ $name =~ $validFirstName ]]
+	then
+			echo "Valid Name"
+	else
+			echo "Invalid Name"
+	fi
+}
+checkValid $firstName
