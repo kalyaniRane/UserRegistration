ae38f550e47ce2d08ceaf1c0654e93d34e45284e
kalynaiRane <kalyanirane19@gmail.com>
2020-02-17 12:16:02 +0530

    ADDED : Function To Check The Valid User's Email Address

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -4,22 +4,36 @@ shopt -s extglob
 echo "Welcome To RegEx Problems"
 
 validName="^[A-Z][a-z]{3,}$"
+validEmail="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
 
-echo "Enter First Name: "
-read firstName
-
-function checkValid()
-{
+function checkValid(){
 	local name=$1
 	if [[ $name =~ $validName ]]
 	then
-			echo "Valid Name"
+			echo "Your Name is Valid."
 	else
-			echo "Invalid Name"
+			echo "Plz Enter Valid Name."
+			exit
 	fi
 }
 
+function checkValidEmail(){
+
+	local email=$1
+	if [[ $email =~ $validEmail ]]
+	then
+			echo "Your Email is Valid."
+	else
+			echo "Plz Enter Valid Email ID."
+	fi
+}
+
+echo "Enter First Name: "
+read firstName
 checkValid $firstName
 echo "Enter Last Name: "
 read lastName
 checkValid $lastName
+echo "Enter Email-ID: "
+read emailId
+checkValidEmail $emailId
